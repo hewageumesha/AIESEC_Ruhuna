3ae04d2c1061c2aea502fcb615c3a24d264ea074
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-04-02 22:15:13 +0530

    controller

--- invoice create/backend/demo/src/main/java/com/invoice/demo/InvoiceGeneratorApplication.java
+++ invoice create/backend/demo/src/main/java/com/invoice/demo/InvoiceGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.invoice.demo.invoicegeneratorapplication;
+package com.invoice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
