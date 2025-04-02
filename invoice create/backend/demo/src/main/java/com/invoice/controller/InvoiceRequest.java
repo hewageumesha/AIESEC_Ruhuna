9e0c33fbccc0fc1530ef8c09a43f57e23d2f7b8f
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-04-02 22:35:42 +0530

    request

---
+++ invoice create/backend/demo/src/main/java/com/invoice/controller/InvoiceRequest.java
@@ -0,0 +1,12 @@
+package com.invoice.controller;
+
+public class InvoiceRequest {
+    public String invoiceNo;
+    public String date;
+    public String dueDate;
+    public String description;
+    public String projectFee;
+    public String volunteerName;
+    public String aiesecEntity;
+    public String receiverSignature;
+}
