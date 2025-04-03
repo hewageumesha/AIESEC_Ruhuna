90f492bd9f80ef8f3f86bb507c3aed74f5e88f93
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-04-03 10:59:11 +0530

    invoice

---
+++ finance_mangement/src/main/java/com/BirthdayMgmtApplication.java
@@ -0,0 +1,11 @@
+package com.birthdaymgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayMgmtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayMgmtApplication.class, args);
+    }
+}
