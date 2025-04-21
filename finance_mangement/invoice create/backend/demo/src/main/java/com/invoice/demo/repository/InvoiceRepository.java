c182589e993d6f04fc9404038ad3a3c3b140c379
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-04-21 19:33:17 +0530

    delete

--- finance_mangement/invoice create/backend/demo/src/main/java/com/invoice/demo/repository/InvoiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.invoice.demo.repository;
-
-import com.invoice.demo.model.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-}
--- finance_mangement/src/main/java/com/birthdaymgnt/BirthdayController.java
+++
@@ -1,5 +0,0 @@
-package main.java.com.birthdaymgnt;
-
-public class BirthdayController {
-    
-}
