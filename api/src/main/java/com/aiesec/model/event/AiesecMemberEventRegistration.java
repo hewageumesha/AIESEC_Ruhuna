ce4f6a36e9c52d03713a4b8600cca0cd7444ee60
Ishara Palangasinghe <145181123+ishara425@users.noreply.github.com>
2025-05-07 10:12:11 +0530

    update

--- api/src/main/java/com/aiesec/model/event/AiesecMemberEventRegistration.java
+++ api/src/main/java/com/aiesec/model/event/AiesecMemberEventRegistration.java
@@ -3,7 +3,9 @@ package com.aiesec.model.event;
 
 import com.aiesec.enums.InterestStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
 
+@Getter
 @Entity
 public class AiesecMemberEventRegistration {
     @Id
@@ -16,40 +18,20 @@ public class AiesecMemberEventRegistration {
     private InterestStatus interestStatus;
     private String comment;
 
-    public Long getId() {
-        return id;
-    }
-
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public Long getUserId() {
-        return UserId;
     }
 
     public void setUserId(Long userId) {
         UserId = userId;
     }
 
-    public Long getEventId() {
-        return EventId;
-    }
-
     public void setEventId(Long eventId) {
         EventId = eventId;
     }
 
-    public InterestStatus getInterestStatus() {
-        return interestStatus;
-    }
-
     public void setInterestStatus(InterestStatus interestStatus) {
         this.interestStatus = interestStatus;
-    }
-
-    public String getComment() {
-        return comment;
     }
 
     public void setComment(String comment) {
--- api/src/main/java/com/aiesec/model/event/GuestEventRegistration.java
+++ api/src/main/java/com/aiesec/model/event/GuestEventRegistration.java
@@ -2,9 +2,13 @@ package com.aiesec.model.event;
 
 import com.aiesec.enums.InterestStatus;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Entity
 
 public class GuestEventRegistration {
@@ -21,69 +25,6 @@ public class GuestEventRegistration {
     private InterestStatus interestStatus;
     private String comment;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Long eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getGuestName() {
-        return guestName;
-    }
-
-    public void setGuestName(String guestName) {
-        this.guestName = guestName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public LocalDateTime getRegistrationDate() {
-        return registrationDate;
-    }
-
-    public void setRegistrationDate(LocalDateTime registrationDate) {
-        this.registrationDate = registrationDate;
-    }
-
-    public InterestStatus getInterestStatus() {
-        return interestStatus;
-    }
-
-    public void setInterestStatus(InterestStatus interestStatus) {
-        this.interestStatus = interestStatus;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
 }
 
 
