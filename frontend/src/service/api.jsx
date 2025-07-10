512bd79c8aac5941d53c9bf4358f446ea01e2e36
Fathima Sajeeya Mohomed Roshan <123949626+fathisaji@users.noreply.github.com>
2025-07-10 06:38:01 +0530

    Update api.jsx

--- frontend/src/service/api.jsx
+++ frontend/src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // Update this based on your backend
+  baseURL: "http://localhost:8080/api", 
 });
 
 // API functions for CRUD operations
