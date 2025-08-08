e054dc5e6921fe87260a26f06a1b0505e958253c
dil <n.u.m.hewage@gmail.com>
2025-08-08 17:16:04 +0530

    update

--- api/src/main/java/com/aiesec/security/WebConfig.java
+++ api/src/main/java/com/aiesec/security/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply to all endpoints
-                .allowedOrigins("*")  // Allow requests from your client
+                .allowedOrigins("https://aiesec-ruhuna.vercel.app/")  // Allow requests from your client
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific methods
                 .allowedHeaders("*");  // Allow all headers
     }
