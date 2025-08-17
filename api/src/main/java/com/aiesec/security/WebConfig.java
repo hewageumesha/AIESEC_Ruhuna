5b76d80c94897c7afd26c3d570cca37bcb69cf6c
Hewage N U M <134699115+hewageumesha@users.noreply.github.com>
2025-08-17 22:53:48 +0530

    Update WebConfig.java

--- api/src/main/java/com/aiesec/security/WebConfig.java
+++ api/src/main/java/com/aiesec/security/WebConfig.java
@@ -9,6 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://aiesec-ruhuna.vercel.app") // your frontend domain
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
             .addResourceHandler("/uploads/**")
